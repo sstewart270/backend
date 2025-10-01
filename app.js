9f2b80c11862b249142b896f95fc7a9c18e8bb3c
Elijah2498 <stewartsamuel2002@gmail.com>
2025-10-01 18:23:20 -0500

    Use process.env.PORT for Render

--- app.js
+++ app.js
@@ -54,6 +54,9 @@ router.put("/songs/:id", async (req, res) => {
 
 app.use("/api", router);
 
-app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
 
